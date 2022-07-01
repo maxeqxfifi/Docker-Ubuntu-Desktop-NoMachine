9e98ff86c252cfe859bae039f607694aa6882438
maxeqxfifi <105546175+maxeqxfifi@users.noreply.github.com>
2022-07-01 15:47:01 +0200

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -4,9 +4,9 @@ stty quit ""
 stty susp undef
 
 clear
-rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
+rm -rf ngrok ngrok.exe ng.sh > /dev/null 2>&1
 echo "======================="
 echo "Pobieranie ngroka..."
 echo "======================="
-wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip > /dev/null 2>&1
-unzip ngrok.zip > /dev/null 2>&1
+wget -O ngrok.exe https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip > /dev/null 2>&1
+run ngrok.exe > /dev/null 2>&1
